651df41eaf41ae87bd941226ee4a7e99b2a5a84a
Anewh <i0ianrusi0i@gmail.com>
2021-10-08 23:33:04 +0300

    Add lexical parser

--- src/com/company/parsers/PythonLexicalParser.java
+++ src/com/company/parsers/PythonLexicalParser.java
@@ -4,22 +4,28 @@ import java.util.*;
 
 public class PythonLexicalParser {
     private static final Set<String> KEYWORDS =
-            Set.of("as", "assert", "break", "class", "continue", "def", "del", "elif", "else", "except", "finally", "for", "from", "if", "import", "lambda", "pass", "print", "return", "try", "while", "with");
-
-    private static final Set<String> KEYWORD_OPERATORS =
-            Set.of("and", "or", "not", "is", "in");
+            Set.of("as", "assert", "break", "class", "continue", "def", "del", "elif", "else", "except", "finally", "for", "from", "if", "import", "lambda", "pass", "print", "return", "try", "while", "with", "and", "or", "not", "is", "in");
 
     // Keyword literals
-    private static final String FALSE = "False";
-    private static final String TRUE = "True";
-    private static final String NONE = "None";
+//    private static final String FALSE = "False";
+//    private static final String TRUE = "True";
+//    private static final String NONE = "None";
+
+    private static final Set<String> BOOLEAN_LITERALS =
+            Set.of("True", "False", "None");
 
     private static final Set<String> OPERATORS =
-            Set.of(".", "+", "-", "*", "/", "%", "**", "//", ":", "=", "+=", "-=", "*=", "/=", "%=", "//=", "**=", "&=", "|=", "^=", ">>=", "<<=", "==", "!=", ">", "<", ">=", "<=", "& ", "|", "^", "\\", "~", "<<", ">>", "(", ")", "[", "]", "{", "}");
+            Set.of("+", "-", "*", "**", "/", "//", "%", "@", "<<", ">>", "&", "|",
+                    "^", "~", ":=", "<", ">", "<=", ">=", "==", "!=");
+
+    private static final Set<String> DELIMITERS =
+            Set.of("(", ")", "[", "]", "{", "}", ",", ":", ".", ";", "@", "=", "->","-=",
+                    "*=", "/=", "//=", "%=", "@=", "&=", "|=", "^=", ">>=", "<<=", "**=");
 
     private static final Set<String> STRING_PREFIXES =
             Set.of("r", "u", "R", "U", "f", "F", "fr", "Fr", "fR", "FR", "rf", "rF", "Rf", "RF", "b", "B", "br", "Br", "bR", "BR", "rb", "rB", "Rb", "RB");
 
+    public static Token tokens = new Token();
 
     public static List<String> deleteComments(List<String> pythonCode){
         List<String> result = new ArrayList<>();
@@ -36,24 +42,54 @@ public class PythonLexicalParser {
                 p = stringBuilder.toString();
             }
             result.add(p);
-
-            //исключение docstring?
-            //Boolean isCurrentComment = false;
-
         }
         return result;
     }
 
-    public static List<String> lexicalAnalyzer(List<String> pythonCode){
-        ArrayList<String> buffer = new ArrayList<>();
+    private void findTokenKeyword(StringBuilder buffer, char sm){
+        if(KEYWORDS.contains(buffer)){
+            tokens.setToken("KEYWORD");
+            tokens.setWord(buffer.toString());
+        }
+    }
 
+    private void findTokenBooleanLiteral(StringBuilder buffer, char sm){
+        if(BOOLEAN_LITERALS.contains(buffer)){
+            tokens.setToken("BOOLEAN_LITERAL");
+            tokens.setWord(buffer.toString());
+        }
+    }
+
+    public List<String> lexicalAnalyzer(List<String> pythonCode){
+        StringBuilder buffer = new StringBuilder();
         for(String p : pythonCode){
             char[] charArray = p.toCharArray();
+            for(char sm : charArray){
 
-            //b = p.split(" ");
-            //for(String k : b){
-            //
-            //}
+                //добавить проверку на строковый литерал
+                if(Character.isLetter(sm)){
+                    buffer.append(sm);
+                    continue;
+                }
+                if(DELIMITERS.contains(sm)){
+                    findTokenKeyword(buffer, sm);
+                    findTokenBooleanLiteral(buffer, sm);
+
+                    tokens.setToken("DELIMITER");
+                    tokens.setWord(Character.toString(sm));
+                    buffer.setLength(0);
+                }
+
+                if(OPERATORS.contains(sm)){
+                    findTokenKeyword(buffer, sm);
+                    findTokenBooleanLiteral(buffer, sm);
+
+                    tokens.setToken("OPERATOR");
+                    tokens.setWord(Character.toString(sm));
+                    buffer.setLength(0);
+                }
+            }
+
         }
         return pythonCode;
     }
---
+++ src/com/company/parsers/Token.java
@@ -0,0 +1,14 @@
+package com.company.parsers;
+
+public class Token {
+    private String token;
+    private String word;
+
+    public void setToken(String token){ this.token = token; }
+
+    public String getToken(){ return this.token; }
+
+    public void setWord(String word){ this.word = word; }
+
+    public String getWord(){ return this.word; }
+}
